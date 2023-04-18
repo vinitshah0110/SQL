6dcd6111dc1b171d8e8111f19c4218f244388875
Vinit Shah <64981658+vinitshah0110@users.noreply.github.com>
2023-04-18 09:40:29 +0530

    Update 1068.sql

--- Leetcode/Easy/1068.sql
+++ Leetcode/Easy/1068.sql
@@ -52,7 +52,35 @@
 -- | Apple        | 2011  | 9000  |
 -- +--------------+-------+-------+
 
+-- Schema:
+-- CREATE TABLE Sales (
+--  `sale_id` INTEGER,
+--  `product_id` INTEGER,
+--  `year` INTEGER,
+--  `quantity` INTEGER,
+--  `price` INTEGER
+-- );
+
+-- INSERT INTO Sales
+-- (`sale_id`, `product_id`, `year`, `quantity`, `price`)
+-- VALUES
+-- ('1', '100', '2008', '10', '5000'),
+-- ('2', '100', '2009', '12', '5000'),
+-- ('7', '200', '2011', '15', '9000');
+
+-- CREATE TABLE Product (
+-- `product_id` INTEGER,
+-- `product_name` VARCHAR(7)
+-- );
+
+-- INSERT INTO Product
+-- (`product_id`, `product_name`)
+-- VALUES
+-- ('100', 'Nokia'),
+-- ('200', 'Apple'),
+-- ('300', 'Samsung');
+
 -- Solution
-select product_name, year, price
-from Sales join Product
-on Sales.product_id = Product.product_id
+SELECT Product.product_name, Sales.year, Sales.price
+FROM Product join Sales
+on Product.product_id = Sales.product_id;
